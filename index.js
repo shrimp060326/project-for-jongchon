ec680ffdb63fd3536fa10d8fd708356c2766e1f8
ju_nh326 <leejh060326@naver.com>
2024-07-05 11:10:27 +0900

    거의 다 구현완료

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function c()  {
     location.href = "android_frame.html";
   }
+
+function b()  {
+  location.href = "cstm_route.html";
+}
+
+function a()  {
+  location.href = "cstm_route2.html";
+}
+
+function complete_input() {
+  if (document.getElementById('start').value == '종촌고등학교' && document.getElementById('end').value == '세종 시립 도서관') {
+    location.href = "android_frame3.html";
+  }
+}
+
+function change() {
+  location.href = "android_frame2.html";
+}
